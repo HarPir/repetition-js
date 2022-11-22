17372552bf4f140dfc55c947bac76ddaac7ed103
HarPir <pirumyanharut@gmail.com>
2022-11-22 22:10:18 +0400

    fibonacci solution with only loupe

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+let i;
+let fibonacciArr = [0, 1];
+
+for (let i = 2; i <= 15; i++) {
+    fibonacciArr[i] = fibonacciArr [i - 2] + fibonacciArr [i - 1];
+    console.log(fibonacciArr[i]);
+}
+
+
+
